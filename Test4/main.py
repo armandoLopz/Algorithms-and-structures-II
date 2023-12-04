fee445498a0cbce345fa7c199cf190d635a35f02
armandoLopz <armandolopez0611@gmail.com>
2023-12-04 09:36:25 -0400

    Add first activity in #2 class

---
+++ Test4/main.py
@@ -0,0 +1,32 @@
+#Programar un procedimientos que calcule los números primos entre dos números
+# sean introducidos por el usuario
+
+numberOne = int(input("Inserte el primer numero:"))
+numberTwo = int(input("Inserte el segundo numero:"))
+
+biggerNumber = 0
+minorNumber = 0
+
+
+def calculateBiggerNumber(one, two):
+
+    if one > two: 
+        biggerNumber = one
+        minorNumber = two
+
+    elif two > one:
+        biggerNumber = two 
+        minorNumber = one
+    else: 
+        biggerNumber = one
+        minorNumber = two
+
+value = 1
+
+for minornumber in biggerNumber:
+
+    for i in biggerNumber 
+
+
+    
+    
---
+++ exercises/ecommerce/main.py
@@ -0,0 +1,55 @@
+class Product:
+
+    def __init__(self,id, name, description, count, amount):
+        
+        self.id = id
+        self.name = name
+        self.description = description
+        self.count = count
+        self.amount = amount
+
+    def datailsProduct(self):
+
+        print(f"Detalles del producto:\nId: {self.id}\nName: {self.name}\nDescripcion: {self.description}\nCantidad: {self.count}\nMonto: {self.amount}")
+
+class User:
+    
+    def __init__(self, id, name, lastname, email, direction):
+        self.id = id
+        self.name = name
+        self.lastname = lastname
+        self.email = email
+        self.direction = direction
+
+class ShoppingCart:
+
+    def __init__(self, products = [], user = "none"):
+
+        self.products = products
+        self.user = user
+
+    def aggProduct(self, product):
+        if product.count > 0 and product.amount > 0 :
+            self.products.append(product)
+            print("Se ha agregado el producto correctamente")
+        else:
+            print("Debe introducir unos datos correctos")
+
+    def showProducts(self):
+        if len(self.products) > 0:
+            for product in self.products:
+                product.datailsProduct()
+        else:
+            print("No hay productos en el carrito")
+
+userShop = User(1, "Armando", "Lopez", "arm@gmail.com", "Naguanagua")
+cart = ShoppingCart([], userShop)
+
+product1y = Product(1, "T shirts", "White t shirts, 100% linen",2,10)
+#cart.aggProduct(product1y)
+
+producty = Product(2, "Jeans", "Cordoury denim jeans", 3, 40)
+
+#cart.aggProduct(product1y)
+
+cart.showProducts()
